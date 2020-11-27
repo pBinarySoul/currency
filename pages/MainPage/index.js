258265cd045564b1d42549d2f1b1a8288089dbc0
pBinarySoul <pBinarySoul@gmail.com>
2020-11-27 09:33:37 -0500

    Main page

--- pages/MainPage/index.js
+++ pages/MainPage/index.js
@@ -1,12 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native";
-import { useSelector } from "react-redux";
+import { Text, View, Image } from "react-native";
+import OurTextButton from "../../components/OurTextButton";
+import styles from "./styles";
+
+const splash = require("../../assets/android-splash-img.png");
 
 const CurrencyPage = (props) => {
-	const state = useSelector(state=>state);
-	
+	const { navigation } = props;
+
+	const goToCurrencyPage = (e) => {
+		navigation.navigate("CurrencyPage");
+	};
 	return (
-		<Text>HELLO</Text>
+		<View style={styles.mainContainer}>
+			<View style={styles.topContainer}>
+				<Text style={styles.title}>Добрый день!</Text>
+				<Text style={styles.text}>Для начала работы нажмите на кнопку Загрузить котировки</Text>
+			</View>
+			<View style={styles.middleContainer}>
+				<Image style={styles.splashImage} source={splash} />
+			</View>
+			<View style={styles.bottomContainer}>
+				<OurTextButton style={{width: 210}} onPress={goToCurrencyPage}>Загрузить котировки</OurTextButton>
+			</View>
+		</View>
 	);
 };
 
---
+++ pages/MainPage/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    topContainer: {
+        paddingHorizontal: 35,
+    },
+    title: {
+        padding: 16,
+        fontSize: 24,
+        color: "#000d",
+        textAlign: "center",
+    },
+    text: {
+        fontSize: 18,
+        color: "#595959dd",
+        textAlign: "center",
+    },
+    middleContainer: {
+        paddingVertical: 40,
+    },
+    splashImage: {
+        width: 224,
+        height: 224,
+    }
+});
+
+export default styles;
